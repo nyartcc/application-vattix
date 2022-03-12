065ade5632d0bdcd3b6946547189535636a452d8
Karl Mathias Moberg <km@kmoberg.no>
2022-03-12 13:49:10 +0100

    Add more verbose output

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from sqlite3 import Error
 from tools import db_init, calc_functions
 import navdata.tools
 from navdata.navdata import load_airac_data
+from classes.vatsim_pilot import Pilot
 
 from vatsim_data.vatsim_data import get_vatsim_data, get_vatsim_status
 
@@ -19,31 +20,40 @@ if __name__ == '__main__':
                                              "skipped to save debug time")
     parser.add_argument("-v", "--verbose", help="(Optional) Add verbose debugging output",
                         default=False, action='store_true')
+    parser.add_argument("-d", "--debug", help="(Optional) Set debug mode; sets certain parameters to default values "
+                                              "and ignores live VATSIM data.", default=False, action='store_true')
 
     # Parse all the available
     args = parser.parse_args()
 
-    # If the user specifies to load navdata (-n), run it!
-     # FIXME Implement: Check if navadata already exist, if not return error requiring the user to input it.
+    verbose = args.verbose
+    debug = args.debug
 
-    print(args.navdata)
+    # If the user specifies to load navdata (-n), run it!
+    # FIXME Implement: Check if navadata already exist, if not return error requiring the user to input it.
+
+    print("Loading data...")
     # Check if we're in production or not.
     if os.environ['env'] != 'prod':
         # Check if the dev db exists, if not, create one.
         if not os.path.isfile('dev.db'):
-            print("Database not found... Creating...")
+            if verbose:
+                print("Database not found... Creating...")
             db, db_name = db_init.sql_connection()
             try:
                 db_file = db_init.sql_table(db)
-                if db_file is True:
+                if db_file is True and verbose is True:
                     print("Success! Created new file " + db_name)
                 else:
-                    print("Failed to create database... 😭 ", db_file[1])
+                    if verbose:
+                        print("Failed to create database... 😭 ", db_file[1])
 
                 create_tables = navdata.tools.create_base_tables(db)
-                print("Creating base tables...")
+                if verbose:
+                    print("Creating base tables...")
                 if create_tables is True:
-                    print("Success! Tables created successfully! 🎉 ")
+                    if verbose:
+                        print("Success! Tables created successfully! 🎉 ")
                 else:
                     print("❌ ERROR! Failed to create base tables.")
             except Error as e:
@@ -65,30 +75,45 @@ if __name__ == '__main__':
                 os.abort()
 
         else:
-            print("Database exists... Continue.")
+            if verbose:
+                print("Database exists... Continue.")
             if args.navdata is not None:
                 load_data = load_airac_data(args.navdata, args.verbose, args.skip)
                 if load_data is True:
-                    print("Success! Navdata loaded.")
+                    if verbose:
+                        print("Success! Navdata loaded.")
                 else:
                     print("❌ ERROR! Failed to load navdata...", load_data[1])
+    print("Data loaded OK.")
 
     statusJson = get_vatsim_status()
     data = statusJson["v3"].pop()
 
     live_data, status = get_vatsim_data(args.verbose)
 
-    print("Data Status:" + status)
+    if verbose:
+        print("Data Status:" + status)
+
+    print("------------------------------------------------------------------\n")
 
     pilot1 = live_data["pilots"][0]
     pilot2 = live_data["pilots"][1]
 
     d = calc_functions.distanceBetweenCoordinates(pilot1["latitude"], pilot1["longitude"],
-                                                  pilot2["latitude"], pilot2["longitude"])
+                                                  pilot2["latitude"], pilot2["longitude"], False, True)
 
     # Todo: Calculate distance a pilot has travelled since the last update.
     # Need to check their current position against the last position.
     # Also need to check distance from origin and destination.
 
+    if debug:
+        debug = pilot1["latitude"], pilot1["longitude"], pilot2["latitude"], pilot2["longitude"]
+
     s = "The distance between {} and {} is {} nautical miles".format(pilot1["callsign"], pilot2["callsign"], round(d))
+
     print(s)
+
+    print(pilot1)
+
+    thing = Pilot.from_dict(pilot1)
+    print(thing.callsign)
