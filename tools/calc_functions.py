e59a8faecb062aeb5b3931b07e1c1d1e7ef0468e
Karl Mathias Moberg <km@kmoberg.no>
2022-03-15 12:37:49 +0100

    Add Trevors improved distance formula. Refactor distance_between_coordinates to use built in radians and use global debug and verbose

--- tools/calc_functions.py
+++ tools/calc_functions.py
@@ -1,20 +1,21 @@
-from math import acos, sin, cos
+from math import acos, sin, cos, radians, atan2, sqrt
+
+from main import debug, verbose
 
 
-def distanceBetweenCoordinates(lat1, lon1, lat2, lon2, debug, verbose):
-    if debug is True:
-        lat1 = 58.964432
-        lat2 = 59.913868
-        lon1 = 5.726250
-        lon2 = 10.752245
+def distance_between_coordninates(src, dst):
+    if debug and verbose is True:
+        lat1 = radians(58.964432)
+        lat2 = radians(59.913868)
+        lon1 = radians(5.726250)
+        lon2 = radians(10.752245)
+    else:
+        lat1 = radians(src['lat'])
+        lon1 = radians(src['lon'])
+        lat2 = radians(dst['lat'])
+        lon2 = radians(dst['lon'])
 
-    PI = 3.141592
-    lat1 = cal_radian(lat1)
-    lat2 = cal_radian(lat2)
-    lon1 = cal_radian(lon1)
-    lon2 = cal_radian(lon2)
-
-    if debug or verbose is True:
+    if debug and verbose is True:
         print(debug, lat1, lon1, lat2, lon2)
         print("Debug: 3963 * acos(sin({lat1}) * sin({lat2}) + cos({lat1}) * cos({lat2}) * cos({lon2} - {lon1})".format(
             lat1=lat1, lon1=lon1, lat2=lat2, lon2=lon2))
@@ -24,15 +25,28 @@ def distanceBetweenCoordinates(lat1, lon1, lat2, lon2, debug, verbose):
     # Convert statute miles to nautical miles
     d = d * 0.87
 
-    if debug is True:
+    if debug and verbose is True:
         print("Control (Should be 164):", round(d))
 
     return d
-
-    # D = 3963 * acos( sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(lon2 – lon1) );
 
 
 def cal_radian(coordinate):
     PI = 3.141592
     x = coordinate * (PI / 180)
     return x
+
+
+def distance_points(src, dst):
+    lat1 = radians(src['lat'])
+    lon1 = radians(src['lon'])
+    lat2 = radians(dst['lat'])
+    lon2 = radians(dst['lon'])
+    print("🐛 Debug - src['lat']", lat1, lon1, lat2, lon2)
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = (sin(dlat / 2)) ** 2 + cos(lat1) * cos(lat2) * (sin(dlon / 2)) ** 2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    # distance = R * c
+    distance = "Fucked"
+    return distance / 1.852
