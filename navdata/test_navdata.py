65556f765233e392268d59306f798b013e194822
Karl Mathias Moberg <km@kmoberg.no>
2022-04-04 12:33:24 +0200

    🎉 Improve tests

--- navdata/test_navdata.py
+++ navdata/test_navdata.py
@@ -1,12 +1,13 @@
-from navdata import Airport, Country
+from navdata.classes import Airport, Country
 
 
 def test_field_access():
-    a = Airport("ENZV", "Stavanger", 54.1, -12, "SVG", "ENSV", 0, 123)
+    a = Airport("ENZV", "Stavanger", 54.1, -12, {"latitude": 54.1, "longitude": -12}, "SVG", "ENSV", 0, 123)
     assert a.icao == "ENZV"
     assert a.name == "Stavanger"
     assert a.latitude == 54.1
     assert a.longitude == -12
+    assert a.position == {"latitude": 54.1, "longitude": -12}
     assert a.iata == "SVG"
     assert a.fir == "ENSV"
     assert a.is_pseudo == 0
@@ -26,30 +27,31 @@ def test_defaults():
 
 
 def test_equality():
-    a1 = Airport("ENZV", "Stavanger", 54.1, -12, "SVG", "ENSV", 0, 123)
-    a2 = Airport("ENZV", "Stavanger", 54.1, -12, "SVG", "ENSV", 0, 123)
+    a1 = Airport("ENZV", "Stavanger", 54.1, -12, {"latitude": 54.1, "longitude": -12}, "SVG", "ENSV", 0, 123)
+    a2 = Airport("ENZV", "Stavanger", 54.1, -12, {"latitude": 54.1, "longitude": -12}, "SVG", "ENSV", 0, 123)
     assert a1 == a2
 
 
 def test_quality_with_diff_ids():
-    a1 = Airport("ENZV", "Stavanger", 54.1, -12, "SVG", "ENSV", 0, 123)
-    a2 = Airport("ENZV", "Stavanger", 54.1, -12, "SVG", "ENSV", 0, 4567)
+    a1 = Airport("ENZV", "Stavanger", 54.1, -12, {"latitude": 54.1, "longitude": -12}, "SVG", "ENSV", 0, 123)
+    a2 = Airport("ENZV", "Stavanger", 54.1, -12, {"latitude": 54.1, "longitude": -12}, "SVG", "ENSV", 0, 4567)
     assert a1 == a2
 
 
 def test_inequality():
-    a1 = Airport("ENZV", "Stavanger", 54.1, -12, "SVG", "ENSV", 0, 123)
-    a2 = Airport("KJFK", "Kennedy Intl.", 40.3, 73.4, "JFK", "KZNY", 0, 123)
+    a1 = Airport("ENZV", "Stavanger", 54.1, -12, {"latitude": 54.1, "longitude": -12}, "SVG", "ENSV", 0, 123)
+    a2 = Airport("KJFK", "Kennedy Intl.", 40.3, 73.4, {"latitude": 40.3, "longitude": 73.4}, "JFK", "KZNY", 0, 123)
     assert a1 != a2
 
 
 def test_from_dict():
-    a1 = Airport("ENZV", "Stavanger", 54.1, -12, "SVG", "ENSV", 0, 123)
+    a1 = Airport("ENZV", "Stavanger", 54.1, -12, {"latitude": 54.1, "longitude": -12}, "SVG", "ENSV", 0, 123)
     a2_dict = {
         "icao": "ENZV",
         "name": "Stavanger",
         "latitude": 54.1,
         "longitude": -12,
+        "position": {"latitude": 54.1, "longitude": -12},
         "iata": "SVG",
         "fir": "ENSV",
         "is_pseudo": 0,
@@ -58,15 +60,15 @@ def test_from_dict():
     a2 = Airport.from_dict(a2_dict)
     assert a1 == a2
 
-
 def test_to_dict():
-    a1 = Airport("ENZV", "Stavanger", 54.1, -12, "SVG", "ENSV", 0, 123)
+    a1 = Airport("ENZV", "Stavanger", 54.1, -12, {"latitude": 54.1, "longitude": -12}, "SVG", "ENSV", 0, 123)
     a2 = a1.to_dict()
     a2_expected = {
         "icao": "ENZV",
         "name": "Stavanger",
         "latitude": 54.1,
         "longitude": -12,
+        "position": {"latitude": 54.1, "longitude": -12},
         "iata": "SVG",
         "fir": "ENSV",
         "is_pseudo": 0,
